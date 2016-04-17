b306865c4a24d7a966904634aba35a6b225765fc
cra5370 <cra5370@rit.edu>
2016-04-17 16:58:34 -0400

    Initial player and movement and stuff

---
+++ unity-fish/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float movementSpeed;
+    public float jumpSpeed;
+    public bool grounded;
+    public float airDrag; // Divides movementSpeed by this number
+
+
+    private float moveVel;
+    private Rigidbody rb;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
+
+        if (grounded)
+        {
+            rb.AddForce(movement * movementSpeed);
+        } else
+        {
+            rb.AddForce(movement * movementSpeed / airDrag);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpSpeed, rb.velocity.z);
+        }
+    }
+
+
+
+    void OnTriggerEnter()
+    {
+        grounded = true;
+    }
+    void OnTriggerExit()
+    {
+        grounded = false;
+    }
+    
+}
